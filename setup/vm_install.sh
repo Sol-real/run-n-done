5a3cf4a2fc98de444612d693059bbac85a6920d6
white-hood <37932436+white-hood@users.noreply.github.com>
2019-11-27 14:58:25 +0100

    Update vm_install.sh

--- setup/vm_install.sh
+++ setup/vm_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib";
+#Testing for root folder
+if [ $UID -ne 0 ]
+then
+  	RED "[-] Not running as root" 
+	exit
+fi
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
 sudo apt update && sudo apt install virtualbox-6.0
