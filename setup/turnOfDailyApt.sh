b17b26d7bfdf44855ce477bf2fc21527ef04b59b
white-hood <darkmaster2210@gmail.com>
2019-12-18 09:46:15 +0100

    Added turnOfDailyApt.sh

---
+++ setup/turnOfDailyApt.sh
@@ -0,0 +1,6 @@
+sudo systemctl disable apt-daily.service
+sudo systemctl disable apt-daily.timer
+
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl disable apt-daily-upgrade.service
+
